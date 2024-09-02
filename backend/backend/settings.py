9071f9873e56920678d5b119a2924627479b79b8
fvizpal <vishalp9966@gmail.com>
2024-09-02 10:12:56 +0530

    refactor:update redirect activation and reset route

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -187,9 +187,9 @@ REST_FRAMEWORK = {
 }
 
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': 'password-reset/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'auth/password-reset/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
-    'ACTIVATION_URL': 'activation/{uid}/{token}',
+    'ACTIVATION_URL': 'auth/activation/{uid}/{token}',
     'USER_CREATE_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'TOKEN_MODEL': None,
